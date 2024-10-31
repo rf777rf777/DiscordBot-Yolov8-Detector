aef3584796955bae3b53e6faff24f47fcf5d61fc
rf777rf777 <rf777rf777@gmail.com>
2024-10-31 16:54:16 +0800

    add context menu function

--- source/cogs/commands.py
+++ source/cogs/commands.py
@@ -10,20 +10,20 @@ class Commands(commands.Cog):
 
     @commands.command()
     async def hello(self, ctx):
-        await ctx.send('Hello from command!')
-        
+        await ctx.send('Hello from command!') 
+    
     @commands.command('d')
     async def detect(self, ctx):
         #Check attachments
         if not ctx.message.attachments:         
-            await ctx.send(embed=self.__getMessageEmbed(f"Please upload an image."))
+            await ctx.send(embed=getMessageEmbed(f"Please upload an image."))
             return
         
         all_detect_result_stream: list[io.BytesIO] = []
         for item in ctx.message.attachments:
-            if not self.__isImageFile(item.filename):
+            if not isImageFile(item.filename):
                 continue
-            await ctx.send(embed=self.__getMessageEmbed(f"{item.filename}: Detecting"))
+            await ctx.send(embed=getMessageEmbed(f"{item.filename}: Detecting"))
             
             #Read image data
             img_data = await item.read()
@@ -31,40 +31,91 @@ class Commands(commands.Cog):
             #Get PIL image
             try:                      
                 image = Image.open(io.BytesIO(img_data))
-                if self.__isPNGFile(item.filename):
+                if isPNGFile(item.filename):
                     image = image.convert('RGB') #if there are 4 channels(RGBA) covert to 3 channels(RGB)
             except:    
-                await ctx.send(embed=self.__getMessageEmbed(f"{item.filename}: Get PIL error", "⛔"))    
+                await ctx.send(embed=getMessageEmbed(f"{item.filename}: Get PIL error", "⛔"))    
                 continue
 
             result_stream = yolov8_service().detect_object_info(image, item.filename)
             if result_stream is None:
-                await ctx.send(embed=self.__getMessageEmbed(f"{item.filename}: Detected nothing", "😓"))
+                await ctx.send(embed=getMessageEmbed(f"{item.filename}: Detected nothing", "😓"))
                 continue  
 
             all_detect_result_stream.append(result_stream)
 
         if len(all_detect_result_stream) <= 0:
+            await ctx.send(embed=getMessageEmbed(f"{item.filename}: Detected nothing", "😓"))
             return
 
         files = [discord.File(fp=stream, filename=f'detected_{i}.jpg') for i, stream in enumerate(all_detect_result_stream)]
-        await ctx.send(embed=self.__getMessageEmbed("Detected", "😎"), files=files)   
+        await ctx.send(embed=getMessageEmbed("Detected", "😎"), files=files)   
 
-    def __isPNGFile(self, filename:str) -> bool:
-        if filename.lower().endswith(('png')):
-            return True
-        return False
+@discord.app_commands.context_menu(name="Detect Images")
+async def detect_image_context(interaction: discord.Interaction, message: discord.Message):
+    #Check attachments
+    if not message.attachments:   
+        await interaction.response.send_message(embed=getMessageEmbed("No Picture to Detect"), ephemeral=True)      
+        return
+    
+    #cog = interaction.client.get_cog("Commands")
+    #if cog:
+        #await interaction.response.send_message(content="Testing", ephemeral=True)
+        #await cog.detect(interaction, message)
+            
+    all_detect_result_stream: list[io.BytesIO] = []
+    
+    await interaction.response.defer()
+
+    for item in message.attachments:
+        if not isImageFile(item.filename):
+            continue
+        #await interaction.response.send_message(embed=__getMessageEmbed(f"{item.filename}: Detecting"))
+        await interaction.followup.send(embed=getMessageEmbed(f"{item.filename}: Detecting"))
+            
+        #Read image data
+        img_data = await item.read()
+
+        #Get PIL image
+        try:                      
+            image = Image.open(io.BytesIO(img_data))
+            if isPNGFile(item.filename):
+                image = image.convert('RGB') #if there are 4 channels(RGBA) covert to 3 channels(RGB)
+        except:    
+            await interaction.followup.send(embed=getMessageEmbed(f"{item.filename}: Get PIL error", "⛔"), ephemeral=True)    
+            continue
+
+        result_stream = yolov8_service().detect_object_info(image, item.filename)
+        if result_stream is None:
+            await interaction.followup.send(embed=getMessageEmbed(f"{item.filename}: Detected nothing", "😓"), ephemeral=True)            
+            continue  
         
-    def __isImageFile(self, filename:str) -> bool:
-        if filename.lower().endswith(('png', 'jpg', 'jpeg')):
-          return True
-        return False
-      
-    def __getMessageEmbed(self, message:str, icon:str="🔊") -> discord.Embed:
-        return discord.Embed(
-            description=f"{icon} {message}",
-            color=discord.Color.from_rgb(184, 134, 11)
-        )
+        all_detect_result_stream.append(result_stream)
 
+    if len(all_detect_result_stream) <= 0:
+        await interaction.followup.send(embed=getMessageEmbed(f"{item.filename}: Detected nothing", "😓"), ephemeral=True)            
+        return
+
+    files = [discord.File(fp=stream, filename=f'detected_{i}.jpg') for i, stream in enumerate(all_detect_result_stream)]
+    await interaction.followup.send(embed=getMessageEmbed("Detected", "😎"), files=files)
+    
 async def setup(bot):
     await bot.add_cog(Commands(bot))
+    # 將上下文菜單指令加入到 bot 的 tree 中
+    bot.tree.add_command(detect_image_context)
+
+def isPNGFile(filename:str) -> bool:
+    if filename.lower().endswith(('png')):
+        return True
+    return False
+        
+def isImageFile(filename:str) -> bool:
+    if filename.lower().endswith(('png', 'jpg', 'jpeg')):
+        return True
+    return False
+      
+def getMessageEmbed(message:str, icon:str="🔊") -> discord.Embed:
+    return discord.Embed(
+        description=f"{icon} {message}",
+        color=discord.Color.from_rgb(184, 134, 11)
+    )
